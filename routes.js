d515267d586ae5c2c59b9289a12fd8cb2892ff22
Spiff Jekey-Green <spiffjekeygreen@gmail.com>
2022-03-25 11:58:04 +0100

    :fire: Added login and register functionality

--- routes.js
+++ routes.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const { User } = require("./models");
+const { authenticate } = require("./util");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET || "secretkldjfal";
 
-// Index route
-router.get("/", (req, res) => {
+// Index routenull
+router.get("/", authenticate, (req, res) => {
+    console.log(req.user);
     res.render("index");
 });
 
 router.get("/login", (req, res) => {
-    res.render("login");
+    res.render("login", { message: undefined });
 });
 
 router.get("/logout", (req, res) => {
@@ -16,7 +21,7 @@ router.get("/logout", (req, res) => {
 })
 
 router.get("/register", (req, res) => {
-    res.render("register");
+    res.render("register", { message: undefined });
 });
 
 router.get("/admin", (req, res) => {
@@ -27,8 +32,47 @@ router.get("*", (req, res) => {
     res.send("404 - Resource not Found");
 });
 
-router.post("/login", (req, res) => {});
+router.post("/login", async (req, res) => {
+    const { password, email } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if(!user) return res.render("login", { message: "User doesn't exist" });
 
-router.post("/registration", (req, res) => {});
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) return res.render("login", { message: "Invalid Credential" });
+        const payload = {
+            user: {id: user.id, role: user.role}
+        }
+        const token = jwt.sign(payload, JWT_SECRET);
+        console.log(token);
+        res.redirect("/");
+    } catch (err) {
+        console.log(err);
+        res.render("login", { message: err.message });
+    }
+});
+
+router.post("/register", async (req, res) => {
+    const { name, email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+        if(user) {
+            return res.render("register", { message: "User already exists" });
+        }
+        user = new User({
+            name,
+            email,
+            password,
+        });
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+        await user.save();
+        return res.render("register", { message: "Successfully created user"});
+    } catch (err) {
+        console.error(err);
+        return res.status(500).render("register", { message: "Server Error"});
+    }
+});
 
 module.exports = router;
--- util.js
+++ util.js
@@ -1,8 +1,31 @@
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET || "secretkldjfal"
+
 const constants = {
     ADMIN_USER: "ADMIN_USER",
     BASIC_USER: "BASIC_USER"
 };
 
-module.exports = {
-    constants
-};
+const authenticate = async (req, res, next) => {
+    const token = req.header("x-auth-token");
+    if (!token) {
+      return res
+        .status(401)
+        .redirect("/login")
+        // .json({ success: false, msg: "No token, authorization denied" });
+    }
+  
+    try {
+      const decoded = jwt.verify(token, JWT_SECRET);
+      req.user = decoded.user;
+      next();
+    } catch (err) {
+        res.redirect("/login");
+    //   res.status(401).json({ success: false, msg: "Token is invalid" });
+    }
+  };
+  
+  module.exports = {
+    constants,
+    authenticate
+  };
